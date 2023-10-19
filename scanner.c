c836d969391c8319ba6a4d956fc03d1e75f46e01
ValesJakub <kubavales39@gmail.com>
2023-10-19 20:58:47 +0200

    Funkce pro pohodlnejsi testovani

--- scanner.c
+++ scanner.c
@@ -14,6 +14,130 @@ fills it and returns NO_ERROR. In case of an error, the return value is LEX_ERRO
 #include "scanner.h"
 // #include <unistd.h>
 
+void printTokenName(T_token token) {
+    switch (token.type) {
+        case TOKEN_ERROR:
+            printf("TOKEN_ERROR");
+            break;
+        case TOKEN_EOF:
+            printf("TOKEN_EOF");
+            break;
+        case TOKEN_COMMA:
+            printf("TOKEN_COMMA");
+            break;
+        case TOKEN_COLON:
+            printf("TOKEN_COLON");
+            break;
+        case TOKEN_PLUS:
+            printf("TOKEN_PLUS");
+            break;
+        case TOKEN_MINUS:
+            printf("TOKEN_MINUS");
+            break;
+        case TOKEN_FUNCTION_TYPE:
+            printf("TOKEN_FUNCTION_TYPE");
+            break;
+        case TOKEN_MUL:
+            printf("TOKEN_MUL");
+            break;
+        case TOKEN_QUESTION_MARK:
+            printf("TOKEN_QUESTION_MARK");
+            break;
+        case TOKEN_DOUBLE_QUESTION_MARK:
+            printf("TOKEN_DOUBLE_QUESTION_MARK");
+            break;
+        case TOKEN_UNDERSCORE:
+            printf("TOKEN_UNDERSCORE");
+            break;
+        case TOKEN_ID:
+            printf("TOKEN_ID");
+            break;
+        case TOKEN_TYPE_ID:
+            printf("TOKEN_TYPE_ID");
+            break;
+        case TOKEN_EXCLAMATION_MARK:
+            printf("TOKEN_EXCLAMATION_MARK");
+            break;
+        case TOKEN_NOT_EQUAL:
+            printf("TOKEN_NOT_EQUAL");
+            break;
+        case TOKEN_ASSIGN:
+            printf("TOKEN_ASSIGN");
+            break;
+        case TOKEN_EQUAL:
+            printf("TOKEN_EQUAL");
+            break;
+        case TOKEN_GT:
+            printf("TOKEN_GT");
+            break;
+        case TOKEN_GTE:
+            printf("TOKEN_GTE");
+            break;
+        case TOKEN_LT:
+            printf("TOKEN_LT");
+            break;
+        case TOKEN_LTE:
+            printf("TOKEN_LTE");
+            break;
+        case TOKEN_L_CURL:
+            printf("TOKEN_L_CURL");
+            break;
+        case TOKEN_R_CURL:
+            printf("TOKEN_R_CURL");
+            break;
+        case TOKEN_L_SQR:
+            printf("TOKEN_L_SQR");
+            break;
+        case TOKEN_R_SQR:
+            printf("TOKEN_R_SQR");
+            break;
+        case TOKEN_R_RND:
+            printf("TOKEN_R_RND");
+            break;
+        case TOKEN_L_RND:
+            printf("TOKEN_L_RND");
+            break;
+        case TOKEN_SINGLE_QUOTE:
+            printf("TOKEN_SINGLE_QUOTE");
+            break;
+        case TOKEN_SLASH:
+            printf("TOKEN_SLASH");
+            break;
+        case TOKEN_BSLASH:
+            printf("TOKEN_BSLASH");
+            break;
+        case TOKEN_STRING:
+            printf("TOKEN_STRING");
+            break;
+        case TOKEN_ML_STRING:
+            printf("TOKEN_ML_STRING");
+            break;
+        case TOKEN_INT:
+            printf("TOKEN_INT");
+            break;
+        case TOKEN_INT_EXP:
+            printf("TOKEN_INT_EXP");
+            break;
+        case TOKEN_INT_EXP_PM:
+            printf("TOKEN_INT_EXP_PM");
+            break;
+        case TOKEN_DOUBLE:
+            printf("TOKEN_DOUBLE");
+            break;
+        case TOKEN_DOUBLE_EXP:
+            printf("TOKEN_DOUBLE_EXP");
+            break;
+        case TOKEN_DOUBLE_EXP_PM:
+            printf("TOKEN_DOUBLE_EXP_PM");
+            break;
+        default:
+            printf("Unknown Token Type");
+            break;
+    }
+    if(token.valueLength)
+        printf("->%s", token.value);
+}
+
 int main(int argc, char *argv[]) {
     
     if(argc < 2){
@@ -33,7 +157,9 @@ int main(int argc, char *argv[]) {
         if(token.type == TOKEN_ERROR){
             printf("chyba\n");
         } else {
-            printf("%d\n", token.type);
+            //printf("%d\n", token.type);
+            printTokenName(token);
+            printf("\n");
         }
     }
     if(token.type == TOKEN_EOF){
