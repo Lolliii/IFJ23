45ada6b96deb1a66a25d8187c26bc868f82667df
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2023-11-20 19:21:32 +0100

    pridani parametru funkci

--- parser.h
+++ parser.h
@@ -20,22 +20,22 @@ PARSER
 // budem potrebovat keywordy z getNextToken v scanneru, typ float int a string nevim ci je potreba
 
 //terminaly z LL gramatiky
-bool prog(T_token token);
-bool st_list(T_token token);
-bool body(T_token token);
-bool stat(T_token token);
-bool ret_stat(T_token token);
-bool id_type(T_token token);
-bool assign(T_token token);
-bool id_stat(T_token token);
-bool call(T_token token);
-bool exp_stat(T_token token);
-bool term_list(T_token token);
-bool t_list(T_token token);
-bool term(T_token token);
-bool f_type(T_token token);
-bool param_list(T_token token);
-bool p_list(T_token token);
-bool param(T_token token);
+bool prog(T_token token, T_queue *queue, FILE *file);
+bool st_list(T_token token, T_queue *queue, FILE *file);
+bool body(T_token token, T_queue *queue, FILE *file);
+bool stat(T_token token, T_queue *queue, FILE *file);
+bool ret_stat(T_token token, T_queue *queue, FILE *file);
+bool id_type(T_token token, T_queue *queue, FILE *file);
+bool assign(T_token token, T_queue *queue, FILE *file);
+bool id_stat(T_token token, T_queue *queue, FILE *file);
+bool call(T_token token, T_queue *queue, FILE *file);
+bool exp_stat(T_token token, T_queue *queue, FILE *file);
+bool term_list(T_token token, T_queue *queue, FILE *file);
+bool t_list(T_token token, T_queue *queue, FILE *file);
+bool term(T_token token, T_queue *queue, FILE *file);
+bool f_type(T_token token, T_queue *queue, FILE *file);
+bool param_list(T_token token, T_queue *queue, FILE *file);
+bool p_list(T_token token, T_queue *queue, FILE *file);
+bool param(T_token token, T_queue *queue, FILE *file);
 
 #endif
