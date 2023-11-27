22da7c507ef52d81860bf3a52d08e37791fef5a2
ValesJakub <kubavales39@gmail.com>
2023-11-27 19:02:24 +0100

    Přidání komentářů

--- expr_stack.h
+++ expr_stack.h
@@ -1,6 +1,8 @@
 /*
 IFJ Projekt 2023
 
+Hlavičkový soubor pro lineární ADT zásobník
+
 @author Jakub Valeš
 @author Milan Takáč
 @author Jakub Dyrčík
@@ -13,7 +15,7 @@ IFJ Projekt 2023
 #include "scanner.h"
 #include "parser_queue.h"
 
-// indexace tokenů v precedenční tabulce (+ je na indexu 0 atd...)
+/* Indexace tokenů v precedenční tabulce (+ je na indexu 0, - na 1 atd...) */
 typedef enum {
     prec_add,
     prec_sub,
@@ -35,8 +37,8 @@ typedef enum {
     prec_que,
     prec_end,
 
-    expressions,
-    e_id,
+    expressions,                    // Pro možnost přeskočení expressions
+    e_id,                           // Po použití pravidla se na vrchol uloží některý typ z expressions
     e_num,
     e_dbl,
     e_str,
@@ -45,27 +47,44 @@ typedef enum {
     e_end,
 } prec_symb;
 
+/* Struktura prvku na zásobníku */
 typedef struct telem
 {
-    T_token token;
-    char *value;
-    prec_symb symb;
-    struct telem *next;
+    T_token token;              // Data prvku (token)
+    char *value;                // Řetězec v token.value (jinak mi to nefungovalo)
+    prec_symb symb;             // Index tokenu v precedenční tabulce
+    struct telem *next;         // Ukazatel na další prvek
 }T_elem;
 
+/* Struktura samotného zásobníku s ukazatelem na vrchol */
 typedef struct tstack
 {
     T_elem *top;
 }T_stack;
 
+/* Funkce pro inicializaci zásobíku (alokoce paměti a nastavení ukazatele na NULL)
+   Bez parametrů
+   Vrací ukazatel na vytvořený zásobník*/
 T_stack *stack_init(void);
 
+/* Funkce pro vložení prvku na vrchol zásobníku
+   Parametry jsou: ukazatel na zásobník, token a jeho hodnota, kterou chceme vložit a index tokenu v precedenční tabulce
+   Bez návratové hodnoty*/
 void stack_push(T_stack *stack, T_token token, char *value, prec_symb idx);
 
+/* Funkce pro odstranění prvku z vrcholu zásobníku
+   Parametrem je ukazatel na zásobník
+   Bez návratové hodnoty*/
 void stack_pop(T_stack *stack);
 
+/* Funkce pro získání prvku z jakéhokoliv místa na zásobníku
+   Parametry jsou: ukazatel na zásobník a index prvku, který chceme získat
+   Vrací ukazatel na prvek zásobníku, když index přesáhne velikost zásobníku, funkce ukončí program*/
 T_elem *stack_get_val(T_stack *stack, int index);
 
+/* Funkce pro vyprázdění zásobníku a uvolnění paměti
+   Parametrem je ukazatel na zásobník
+   Bez návratové hodnoty*/
 void stack_empty(T_stack *stack);
 
 #endif
--- parser_queue.h
+++ parser_queue.h
@@ -1,6 +1,8 @@
 /*
 IFJ Projekt 2023
 
+Hlavičkový soubor pro lineární ADT fronta
+
 @author Jakub Valeš
 @author Milan Takáč
 @author Jakub Dyrčík
@@ -12,30 +14,43 @@ IFJ Projekt 2023
 
 #include "scanner.h"
 
-// indexace tokenů v precedenční tabulce (+ je na indexu 0 atd...)
-
-
+/* Struktura pro prvek ve frontě*/
 typedef struct telem_queue
 {
-    T_token token;
-    struct telem_queue *next;
+    T_token token;                  // Data prvku ve frontě
+    struct telem_queue *next;       // Ukazatel na další prvek
 }T_queue_elem;
 
+/* Struktura pro samotnou frontu s ukazateli na začátek a konec*/
 typedef struct tqueue
 {
     T_queue_elem *beg;
     T_queue_elem *end;
 }T_queue;
 
-
+/* Funkce pro inicializaci fronty, dynamicky naalokuje prostor a nastaví ukazatele na NULL
+   Bez parametrů
+   Vrací ukazatel na vytvořenou frontu*/
 T_queue *queue_init(void);
 
+/* Funkce pro přidání prvku(tokenu) na konec fronty
+   Parametry jsou: ukazatel na frontu, kam chceme prvek přidat a samotný prvek
+   Bez návratové hodnoty*/
 void queue_add(T_queue *queue, T_token token);
 
+/* Funkce pro odebrání prvku z vrcholu fronty
+   Parametrem je ukazatel na frontu
+   Bez návratové hodnoty*/
 void queue_remove(T_queue *queue);
 
+/* Funkce pro získání prvku z vrcholu fronty
+   Parametrem je ukazatel na frontu
+   Vrací token z vrcholu fronty*/
 T_token queue_front(T_queue *queue);
 
+/* Funkce pro získání dalšího tokenu z fronty nebo souboru
+   Parametry jsou ukazatel na frontu a ukazatel na soubor
+   Vrací token*/
 T_token getToken(T_queue *queue, FILE *file);
 
 #endif
